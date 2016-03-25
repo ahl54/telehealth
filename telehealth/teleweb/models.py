f47db0addc5b2937039fb545ead6c8e7f7ad8530
Anna Lu <anna.lu127@gmail.com>
2016-03-25 13:27:28 -0400

    events from cube

--- telehealth/teleweb/models.py
+++ telehealth/teleweb/models.py
@@ -4,11 +4,22 @@ from django.db import models
 
 # Create your models here
 
-# EKG stream
+# continuous temperature stream
 
-# EEG stream 
+class Person(models.Model):
+    created_on = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=255)
+    age = models.IntegerField()
+    email = models.EmailField()
+    text = models.TextField()
 
-# continuous stream temperature
+    def __unicode__(self):
+        return unicode(self.name)
 
-# 
-    .
+class Temperature(models.Model)
+    date_time = models.DateTimeField(blank=True, null=True)
+    temperature = models.DecimalField(max_digits=5, decimal_places=2)
+    basal = models.FloatField(blank=True, null=True)
+
+    def __unicode__(self):
+        return unicode(self.temperature)
